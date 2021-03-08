bd04fb514a4173d9b213b3ecfd176d1fd849c534
HayrullahDemirtas <hayr43demir@gmail.com>
2021-03-08 13:08:35 +0300

    Hello React

---
+++ src/main/java/com/hayr/personal_development_planning/PersonalDevelopmentPlanningApplication.java
@@ -0,0 +1,13 @@
+package com.hayr.personal_development_planning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalDevelopmentPlanningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalDevelopmentPlanningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hayr/personal_development_planning/PersonalDevelopmentPlanningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hayr.personal_development_planning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalDevelopmentPlanningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
